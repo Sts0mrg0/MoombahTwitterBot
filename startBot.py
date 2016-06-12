613a71adbc10c11d075901fad49f947e6e01201b
root <root@aptgetswag.(none)>
2016-06-12 01:23:34 -0400

    Tweaks + autoRT

--- startBot.py
+++ startBot.py
@@ -12,21 +12,26 @@ fetchIntervalMinutes='60'
 #Globals
 autoFavPhrases = open(autoFavPhrasesFile).readlines()
 autoFollowPhrases = open(autoFollowPhrasesFile).readlines()
+autoRetweetPhrases = open(autoRetweetPhrasesFile).readlines()
 
 def fetch(bot):
 	#actions
-	autoFav(bot)
+	autoRetweet(bot)
 	bot.auto_follow_followers()
 	autoFollow(bot)
+	autoFav(bot)
 
 def autoFollow(bot):
 	for phrase in autoFollowPhrases:
-		moombahBot.auto_follow(phrase, count=300)
+		bot.auto_follow(phrase, count=1)
 
 def autoFav(bot):
 	for phrase in autoFavPhrases:
-		moombahBot.auto_fav(phrase, count=300)
-		
+		bot.auto_fav(phrase, count=300)
+
+def autoRetweet(bot):
+	for phrase in autoRetweetPhrases:
+		bot.auto_rt(phrase, count=1)
 if __name__ == "__main__":
 	print "Starting MoombahBot..."
 	
@@ -35,14 +40,14 @@ if __name__ == "__main__":
 
 	#init bot
 	moombahBot = TwitterBot("apiConfig.conf")
-#	moombahBot.sync_follows()
+	moombahBot.sync_follows()
 	fetch(moombahBot)
 
 	#Sync once daily @ 1 AM
-	sched.add_job(lambda: moombahBot.sync_follows(), 'cron', hour='1')
+	sched.add_job(lambda: moombahBot.sync_follows(), 'cron', hour='1', replace_existing=True)
 
 	#Fetch at interval
-	sched.add_job(lambda: fetch(moombahBot), 'cron', minute=fetchIntervalMinutes)
+	sched.add_job(lambda: fetch(moombahBot), 'cron', minute=fetchIntervalMinutes, replace_existing=True)
 
 	#Run until exit
 	print "Moombah Bot Running"
